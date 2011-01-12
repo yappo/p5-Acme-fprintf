38905aadd1a5ecaa77cf461b1bfd0e99210caff1
Kazuhiro Osawa <yappo@shibuya.pl>
2011-01-12 20:15:07 +0900

    initial commit

---
+++ lib/Acme/fprintf.pm
@@ -0,0 +1,32 @@
+package Acme::fprintf;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::fprintf -
+
+=head1 SYNOPSIS
+
+  use Acme::fprintf;
+
+=head1 DESCRIPTION
+
+Acme::fprintf is
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo {at} shibuya {dot} plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
