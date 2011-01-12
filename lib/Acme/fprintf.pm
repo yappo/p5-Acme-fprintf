b6cac1d4d5be544d7c05cf5841cb0743a6f018b9
Kazuhiro Osawa <yappo@shibuya.pl>
2011-01-12 20:42:57 +0900

    added fprintf

--- lib/Acme/fprintf.pm
+++ lib/Acme/fprintf.pm
@@ -2,27 +2,38 @@ package Acme::fprintf;
 use strict;
 use warnings;
 our $VERSION = '0.01';
+our @ISA = 'Exporter';
+our @EXPORT = 'fprintf';
+
+
+sub fprintf (*$;@) {
+    my($fh, $fmt, @args) = @_;
+    print $fh sprintf $fmt, @args;
+}
 
 1;
 __END__
 
 =head1 NAME
 
-Acme::fprintf -
+Acme::fprintf - fprintf for Perl
 
 =head1 SYNOPSIS
 
   use Acme::fprintf;
+  fprintf $fh, '%04d %s', 31, 'yappo'; # 00031 yappo
 
 =head1 DESCRIPTION
 
-Acme::fprintf is
+Acme::fprintf is fprintf(3) for Perl.
 
 =head1 AUTHOR
 
 Kazuhiro Osawa E<lt>yappo {at} shibuya {dot} plE<gt>
 
 =head1 SEE ALSO
+
+fprintf(3), perldoc -f sprintf
 
 =head1 LICENSE
 
